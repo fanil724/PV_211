b8e64b51fa6078916da8b5dbb34f93d3929721d6
Фаниль <fanil724@gmail.com>
2023-08-03 16:28:43 +0300

    Font DONE

--- WinAPI/TextEditor/Main.cpp
+++ WinAPI/TextEditor/Main.cpp
@@ -1,10 +1,20 @@
-﻿//#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include"resource.h"
 #include<RichEdit.H>
+#include<shellapi.h>
+#include<wingdi.h>
+#include<stdio.h>
+#include <string>
 
 CONST CHAR g_sz_CLASS_NAME[] = "Text Editor PV_211";
+CONST CHAR* g_sz_SIZE[] = { "8","9","10","12","14","16","18","20","22","24","26","28","36","48","72" };
+CONST CHAR* g_sz_INSCRIPTION[] = { "обычный","курсив","полужирный","полужирный курсив" };
+CONST CHAR* g_sz_FONT[] = { "Times New Roman","Georgia","Arial","Arial Black","Tahoma",
+"Verdana","Trebuchet MS","Lucida Sans Unicode","Impact","Comic Sans MS","Courier New","Lucida Console" };
+
 LRESULT CALLBACK WndPRoc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcFont(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName);
 BOOL SaveTextFileFromEdit(HWND hEsit, LPCSTR lpszFileName);
 
@@ -54,6 +64,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
     MSG msg;
     while (GetMessage(&msg, 0, 0, 0) > 0) {
+        DragAcceptFiles(hwnd, TRUE);
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
@@ -70,7 +81,7 @@ LRESULT CALLBACK WndPRoc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         GetWindowRect(hwnd, &window_rect);
         GetClientRect(hwnd, &client_rect);
         HWND hEdit = CreateWindowEx(
-            NULL, RICHEDIT_CLASS, "",
+            WS_EX_ACCEPTFILES, RICHEDIT_CLASS, "",
             WS_CHILD | WS_VISIBLE | WS_BORDER | WS_HSCROLL | WS_VSCROLL | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOHSCROLL,
             0, 0,
             client_rect.right, client_rect.bottom,
@@ -82,6 +93,14 @@ LRESULT CALLBACK WndPRoc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
         //  HWND hStatus = CreateWindowEx(0,STATUSCLASSNAME);
 
+    }break;
+    case WM_DROPFILES: {
+        INT SIZE = DragQueryFile((HDROP)wParam, 0, NULL, 0);
+        CHAR* szFileName = new CHAR[SIZE + 1];
+        DragQueryFile((HDROP)wParam, 0, szFileName, SIZE);
+        MessageBox(NULL, szFileName, "title", MB_OK);
+        LoadTextFileToEdit(GetDlgItem(hwnd, IDC_EDIT), szFileName);
+        DragFinish((HDROP)wParam);
     }break;
     case WM_COMMAND: {
         switch (LOWORD(wParam))
@@ -116,6 +135,10 @@ LRESULT CALLBACK WndPRoc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
             if (GetSaveFileName(&ofn))SaveTextFileFromEdit(GetDlgItem(hwnd, IDC_EDIT), ofn.lpstrFile);
         }break;
         case ID_FILE_EXIT:SendMessage(hwnd, WM_CLOSE, 0, 0); break;
+        case ID_FORMAT_FONT: {
+            DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_FORMAT_FONT), hwnd, DlgProcFont, 0);
+        }break;
+
         }
     } break;
     case WM_DESTROY:  PostQuitMessage(0); break;
@@ -124,6 +147,120 @@ LRESULT CALLBACK WndPRoc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     }
     return 0;
 }
+
+BOOL CALLBACK DlgProcFont(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg)
+    {
+    case WM_INITDIALOG: {
+        LoadLibrary("Gdi32.dll");
+        CONST INT SIZE = 256;
+        HWND hListSize = GetDlgItem(hwnd, IDC_LIST3);
+        for (int i = 0; i < sizeof(g_sz_SIZE) / sizeof(g_sz_SIZE[0]); i++)
+        {
+            SendMessage(hListSize, LB_ADDSTRING, 0, (LPARAM)g_sz_SIZE[i]);
+        }
+        HWND hListInscription = GetDlgItem(hwnd, IDC_LIST2);
+        for (int i = 0; i < sizeof(g_sz_INSCRIPTION) / sizeof(g_sz_INSCRIPTION[0]); i++)
+        {
+            SendMessage(hListInscription, LB_ADDSTRING, 0, (LPARAM)g_sz_INSCRIPTION[i]);
+        }
+        HWND hListFont = GetDlgItem(hwnd, IDC_LIST1);
+        for (int i = 0; i < sizeof(g_sz_FONT) / sizeof(g_sz_FONT[0]); i++)
+        {
+            SendMessage(hListFont, LB_ADDSTRING, 0, (LPARAM)g_sz_FONT[i]);
+        }
+
+        LOGFONT lf;
+        ZeroMemory(&lf, sizeof(LOGFONT));
+        HFONT hFont = (HFONT)GetStockObject(ANSI_FIXED_FONT);
+        GetObject(hFont, sizeof(LOGFONT), &lf);
+        CHAR Height[SIZE] = {};
+        CHAR Inscription[SIZE] = {};
+        sprintf(Height, "%d", lf.lfHeight);
+        SendMessage(GetDlgItem(hwnd, IDC_EDIT3), WM_SETTEXT, 0, (LPARAM)Height);
+        SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)lf.lfFaceName);
+        if (lf.lfItalic) {
+            if (lf.lfWeight == FW_BOLD)
+                SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)g_sz_INSCRIPTION[3]);
+            SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)g_sz_INSCRIPTION[1]);
+        }
+        else if (lf.lfWeight == FW_BOLD) {
+            SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)g_sz_INSCRIPTION[2]);
+        }
+        else {
+            SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)g_sz_INSCRIPTION[0]);
+        }
+        SendMessage(hListSize, LB_SETCURSEL, SendMessage(hListSize, LB_FINDSTRING, -1, (LPARAM)Height), 0);
+        SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_GETTEXT, SIZE, (LPARAM)Inscription);
+        SendMessage(hListInscription, LB_SETCURSEL, SendMessage(hListInscription, LB_FINDSTRING, -1, (LPARAM)Inscription), 0);
+        SendMessage(hListFont, LB_SETCURSEL, SendMessage(hListFont, LB_FINDSTRING, -1, (LPARAM)lf.lfFaceName), 0);
+    }break;
+    case WM_COMMAND: {
+        switch (LOWORD(wParam))
+        {
+        case IDOK: {
+            CONST INT SIZE = 20;
+            CHAR sz_SIZE[SIZE] = {};
+            CHAR sz_FONT[SIZE] = {};
+            CHAR sz_Inscription[SIZE] = {};
+            SendMessage(GetDlgItem(hwnd, IDC_EDIT3), WM_GETTEXT, SIZE, (LPARAM)sz_SIZE);
+            SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)sz_FONT);
+            SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_GETTEXT, SIZE, (LPARAM)sz_Inscription);
+            BOOL Italic = FALSE;
+            INT Weight = 400;
+            if (strcmp(sz_Inscription, "обычный") == 0) {
+                Italic = false;
+                Weight = FW_NORMAL;
+            }
+            if (strcmp(sz_Inscription, "курсив") == 0) {
+                Italic = TRUE;
+                Weight = FW_NORMAL;
+            }
+            if (strcmp(sz_Inscription, "полужирный") == 0) {
+                Italic = false;
+                Weight = FW_BOLD;
+            }
+            if (strcmp(sz_Inscription, "полужирный курсив") == 0) {
+                Italic = TRUE;
+                Weight = FW_BOLD;
+            }
+            HFONT hFont = CreateFont(std::stoi(sz_SIZE), 0, 0, 0, Weight, Italic, FALSE, FALSE, ANSI_CHARSET,
+                OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+                DEFAULT_PITCH | FF_DONTCARE, sz_FONT);
+            HWND hParent = GetParent(hwnd);
+            SendMessage(GetDlgItem(hParent, IDC_EDIT), WM_SETFONT, (WPARAM)hFont, TRUE);
+        }
+
+        case IDCANCEL: EndDialog(hwnd, 0); break;
+
+        }
+        HWND hList = (HWND)lParam;
+        if (LBN_SELCHANGE) {
+            CONST INT SIZE = 64;
+            if (hList == GetDlgItem(hwnd, IDC_LIST3)) {
+                CHAR sz_Size[SIZE] = {};
+                SendMessage(GetDlgItem(hwnd, IDC_LIST3), LB_GETTEXT, SendMessage(GetDlgItem(hwnd, IDC_LIST3), LB_GETCURSEL, 0, 0), (LPARAM)sz_Size);
+                SendMessage(GetDlgItem(hwnd, IDC_EDIT3), WM_SETTEXT, SIZE, (LPARAM)sz_Size);
+            }
+            if (hList == GetDlgItem(hwnd, IDC_LIST2)) {
+                CHAR sz_Inscription[SIZE] = {};
+                SendMessage(GetDlgItem(hwnd, IDC_LIST2), LB_GETTEXT, SendMessage(GetDlgItem(hwnd, IDC_LIST2), LB_GETCURSEL, 0, 0), (LPARAM)sz_Inscription);
+                SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, SIZE, (LPARAM)sz_Inscription);
+            }
+            if (hList == GetDlgItem(hwnd, IDC_LIST1)) {
+                CHAR sz_Font[SIZE] = {};
+                SendMessage(GetDlgItem(hwnd, IDC_LIST1), LB_GETTEXT, SendMessage(GetDlgItem(hwnd, IDC_LIST1), LB_GETCURSEL, 0, 0), (LPARAM)sz_Font);
+                SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, SIZE, (LPARAM)sz_Font);
+            }
+        }break;
+    }
+    case WM_CLOSE: EndDialog(hwnd, 0);
+    }
+    return FALSE;
+}
+
+
+
 BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName) {
     BOOL bSuccess = FALSE;
     HANDLE hFile = CreateFile(lpszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
--- WinAPI/TextEditor/resource.h
+++ WinAPI/TextEditor/resource.h
@@ -2,8 +2,17 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
+
 #define IDR_MAIN_MENU                   101
+#define IDD_FORMAT_FONT                 102
+
 #define IDC_EDIT                        1000
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_EDIT3                       1003
+#define IDC_LIST1                       1004
+#define IDC_LIST2                       1005
+#define IDC_LIST3                       1006
 #define ID_FILE_NEW                     40001
 #define ID_FILE_OPEN                    40002
 #define ID_FILE_SAVE                    40003
@@ -16,9 +25,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40008
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
